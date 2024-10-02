59e367a2fde9ab5296e38a58edb3785c68901951
GwonDooHyeon <122503466+GwonDooHyeon@users.noreply.github.com>
2024-10-03 04:20:10 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 // 분해합
 // https://www.acmicpc.net/problem/2231
@@ -12,22 +11,39 @@ public class Main {
 
         int number = Integer.parseInt(br.readLine());
 
-        boolean found = false;
-        for (int i = 0; i < number; i++) {
-            String numberStr = String.valueOf(i);
-            String[] numberStrArr = numberStr.split("");
-            int sum = Arrays.stream(numberStrArr).mapToInt(Integer::parseInt).sum();
+        // 1번 방식 (시간 오래걸림)
+//        boolean found = false;
+//        for (int i = 0; i < number; i++) {
+//            String numberStr = String.valueOf(i);
+//            String[] numberStrArr = numberStr.split("");
+//            int sum = Arrays.stream(numberStrArr).mapToInt(Integer::parseInt).sum();
+//            if (i + sum == number) {
+//                found = true;
+//                System.out.println(i);
+//                break;
+//            }
+//        }
+//        if (!found) {
+//            System.out.println(0);
+//        }
+
+        // 2번 방식
+        int answer = 0;
+
+        for (int i = 1; i < number; i++) {
+            int num = i;
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
             if (i + sum == number) {
-                found = true;
-                System.out.println(i);
+                answer = i;
                 break;
             }
         }
-
-        if (!found) {
-            System.out.println(0);
-        }
-
+        System.out.println(answer);
     }
 
 }
